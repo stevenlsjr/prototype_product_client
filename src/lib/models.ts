3592c068585a71c6d07d37a42da39a2b8d8ca81e
Steve Shea <stevenlsjr@gmail.com>
2019-06-29 16:59:35 -0400

    added json serialization code

--- src/lib/models.ts
+++ src/lib/models.ts
@@ -1,19 +1,57 @@
-import { snakeCase, camelCase } from "lodash";
+import _ from "lodash";
+import { spec, symbol, explainData, conform, explain } from "js.spec";
+
+const monetary = spec.or("monetary", {
+  decimalString: spec.string,
+  number: spec.number
+});
+
+const apiProduct = spec.map("apiProduct", {
+  name: spec.string,
+  upc: spec.string,
+  price_per_item: monetary,
+  [symbol.optional]: {
+    url: spec.string,
+    pk: spec.int,
+    categories: spec.collection("array of urls", spec.string)
+  }
+});
+
+const authUser = spec.map("authUser", {
+  username: spec.string,
+  [symbol.optional]: {
+    url: spec.string,
+    pk: spec.integer,
+    email: spec.string,
+    groups: spec.collection("groups", spec.string),
+    is_staff: spec.boolean
+  }
+});
+
+export const specs = {
+  monetary,
+  apiProduct,
+  authUser
+};
 
 export interface AuthUser {
-  url: string;
-  pk: number;
+  url?: string;
+  pk?: number;
   username: string;
-  email: string;
+  email?: string;
   groupUrls?: [string];
   groups?: [string];
-  isStaff?: [string];
+  isStaff?: boolean;
 }
 
-export interface Jwt {}
+export interface Jwt {
+  accept: string;
+  refresh: string;
+}
 
 export interface Product {
   url?: string;
+  pk: number;
   name: string;
   upc: string;
   categoryUrls?: string[];
@@ -29,13 +67,92 @@ export interface PaginatedList<Model> {
   result: Model[];
 }
 
-export function jsonToProduct(json: any): Product {
-  const { url, pk, name, upc, price_per_item, categories } = json;
-  return {
-    url,
-    name,
-    upc,
-    pricePerItem: price_per_item,
-    categoryUrls: categories
-  };
+export interface StringKeyObject<T> {
+  [str: string]: T;
+}
+
+/**
+ * Converts the keys in an object from snake_case or PascalCase to camelCase
+ * @param obj an object with String type keys
+ */
+export function keysToCamel<T = any>(
+  obj: StringKeyObject<T>
+): StringKeyObject<T> {
+  return _.mapKeys<typeof obj>(obj, (value, key) => _.camelCase(key));
+}
+
+/**
+ * Converts the keys in an object from PascalCase or camelCase to snake_case
+ * @param obj an object with String type keys
+ */
+export function keysToSnake<T = any>(
+  obj: StringKeyObject<T>
+): StringKeyObject<T> {
+  return _.mapKeys<typeof obj>(obj, (value, key) => _.snakeCase(key));
+}
+
+/**
+ *
+ * @param json pojo object representing a product resource
+ * from the python api. Should adhere to specs.apiProduct
+ */
+export function jsonToProduct(json: any & object): Product {
+  if (conform(specs.apiProduct, json) == symbol.invalid) {
+    throw new Error(
+      `data does not conform to product spec: ${explainData(
+        specs.apiProduct,
+        json
+      )}`
+    );
+  }
+
+  const product = _.transform(
+    json,
+    (result: any, value, key: string) => {
+      let newKey: string;
+      if (key === "categories") {
+        newKey = "categoryUrls";
+      } else {
+        newKey = _.camelCase(key);
+      }
+
+      if (newKey === "pricePerItem" && typeof value === "string") {
+        value = parseFloat(value);
+      }
+      result[newKey] = value;
+    },
+    {}
+  ) as Product;
+  return product;
+}
+
+export function productToJson(product: Product): any {
+  let js: any = { categories: product.categoryUrls, ...product };
+  js = keysToSnake(js);
+  return js;
+}
+
+/**
+ * Converts a user instance to api serializable object
+ * @param user user instance
+ */
+export function userToJson(user: AuthUser): any {
+  user.groups = user.groupUrls;
+  user.groupUrls = undefined;
+  return keysToSnake(user);
+}
+
+/**
+ * converts an api serializable user object to a javascript user instance
+ * @param json an api serializable user object
+ */
+export function jsonToUser(json: any): AuthUser {
+  if (conform(specs.authUser, json) === symbol.invalid) {
+    throw new Error(`validation error: ${explainData(specs.authUser, json)}`);
+  }
+  json.groupUrls = json.groups
+  delete json.groups
+  const u = keysToCamel(json) as AuthUser;
+  
+  return u;
 }
--- tests/unit/models.spec.ts
+++ tests/unit/models.spec.ts
@@ -1,3 +1,171 @@
+import {
+  keysToCamel,
+  keysToSnake,
+  jsonToProduct,
+  Product,
+  productToJson,
+  specs,
+  AuthUser,
+  userToJson,
+  jsonToUser
+} from "@/lib/models";
+import { conform, symbol } from "js.spec";
+
 describe("product json entry to js type conversion", () => {
-  test("something", () => {});
+  test("converts objects from snake_case representation to camelCase", () => {
+    const val = keysToCamel({
+      a_value: 10,
+      another_value: "values_arent_converted",
+      PascalCaseVal: {
+        is_not_nested: null
+      }
+    });
+    expect(val).toMatchObject({
+      aValue: 10,
+      anotherValue: "values_arent_converted",
+      pascalCaseVal: {
+        is_not_nested: null
+      }
+    });
+  });
+
+  test("converts camelCase keys to snake_case", () => {
+    const val = keysToSnake({
+      a_value: 10,
+      another_value: "values_arent_converted",
+      PascalCaseVal: {
+        isNotNested: null
+      }
+    });
+    expect(val).toMatchObject({
+      a_value: 10,
+      another_value: "values_arent_converted",
+      pascal_case_val: {
+        isNotNested: null
+      }
+    });
+  });
+});
+
+describe("serializing json product value to javascript api", () => {
+  test("should produce product with valid data", () => {
+    let json: any = {
+      url: "http://localhost:8000/api/v1/products/2/",
+      pk: 2,
+      name: "a hammer",
+      upc: "0002",
+      price_per_item: "0.000",
+      categories: ["http://localhost:8000/api/v1/categories/1/"]
+    };
+    expect(jsonToProduct(json)).toMatchObject({
+      url: "http://localhost:8000/api/v1/products/2/",
+      pk: 2,
+      name: "a hammer",
+      upc: "0002",
+      pricePerItem: 0.0,
+      categoryUrls: ["http://localhost:8000/api/v1/categories/1/"]
+    });
+
+    json = {
+      pk: "ima string",
+      categories: "should be an iterator"
+    };
+
+    expect(() => jsonToProduct(json)).toThrowError();
+  });
+
+  test("converting an invalid instance throws an error", () => {});
+
+  test("converting javascript product to json api", () => {
+    const product: Product = {
+      url: "http://localhost:8000/api/v1/products/2/",
+      pk: 2,
+      name: "a hammer",
+      upc: "0002",
+      pricePerItem: 0.0,
+      categoryUrls: ["http://localhost:8000/api/v1/categories/1/"]
+    };
+    let json = productToJson(product);
+
+    expect(json).toMatchObject({
+      url: "http://localhost:8000/api/v1/products/2/",
+      pk: 2,
+      name: "a hammer",
+      upc: "0002",
+      price_per_item: 0,
+      categories: ["http://localhost:8000/api/v1/categories/1/"]
+    });
+
+    expect(conform(specs.apiProduct, json)).not.toEqual(symbol.invalid);
+  });
+});
+
+describe("serializing and deserializing users", () => {
+  test("converting user to json", () => {
+    const validUsers: AuthUser[] = [
+      { username: "bob" },
+      { username: "bobby", groups: ["admin"] },
+      {
+        username: "bobby",
+        groupUrls: ["/group/1"],
+        groups: ["admin"],
+        isStaff: false,
+        url: "www.google.com",
+        email: "bob@www.google.com"
+      }
+    ];
+    const [a, b, c] = validUsers.map(x => userToJson(x));
+    expect(a).toMatchObject({ username: "bob" });
+    expect(b).toMatchObject({ username: "bobby" }),
+      expect(c).toMatchObject({
+        username: "bobby",
+        groups: ["/group/1"],
+        is_staff: false,
+        url: "www.google.com",
+        email: "bob@www.google.com"
+      });
+  });
+
+  test("converting json to users", () => {
+    const validJson = [
+      {
+        url: "http://localhost:8000/api/v1/users/2/",
+        pk: 2,
+        username: "yogibear",
+        email: "",
+        groups: [],
+        is_staff: false
+      },
+      {
+        url: "http://localhost:8000/api/v1/users/1/",
+        pk: 1,
+        username: "steve",
+        email: "stevenlsjr@email.com",
+        groups: [],
+        is_staff: true
+      }
+    ];
+
+    expect(() => validJson.map(js => jsonToUser(js))).not.toThrow();
+    const invalidJson = [
+      {
+        url: 1,
+        pk: 2,
+        username: "yogibear",
+        email: "",
+        groups: [],
+        is_staff: false
+      },
+      {
+        url: "http://localhost:8000/api/v1/users/1/",
+        pk: 1,
+        email: "stevenlsjr@email.com",
+        groups: [],
+        is_staff: true
+      }
+    ];
+    for (let u of invalidJson) {
+      expect(() => jsonToUser(u)).toThrow();
+    }
+  });
 });
